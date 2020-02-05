9102ac4426d36092865a0a470e9f480e7f71bdd9
gurkerl83 <markus_gritsch@gmx.de>
2020-02-05 20:36:45 +0100

    Before, setup of vanilla Http middleware document middleware of Next. Now, the instantiation of the middleware happens in _document getInitialProps method. Forgot to defined publicRuntimeConfig

--- examples/simple/next.config.js
+++ examples/simple/next.config.js
@@ -1,28 +1,16 @@
 const path = require('path');
 
-// const exportPathMap = require('./dist/next/exportPathMap');
-
 module.exports = {
-  // exportPathMap,
   target: 'serverless',
 
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 
-  experimental: { documentMiddleware: true },
-
   publicRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
-    PROJECT_ROOT_2: 'Hello'
+    rootDir: __dirname
   },
 
-  // webpack: (config, options) => {
   webpack: (config, { isServer }) => {
     config.mode = 'production';
-
-    // Fixes npm packages that depend on `fs` module
-    // config.node = {
-    //   fs: 'empty'
-    // };
 
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
@@ -30,13 +18,6 @@ module.exports = {
         fs: 'empty'
       };
     }
-
-    // The context is two levels out, because next does currently not support
-    // configurations (next.config) in typescript
-    // config.context = path.resolve(__dirname, '../../');
-    // config.resolve = {
-    //   extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-    // };
 
     return config;
   }
--- examples/simple/pages/_document.js
+++ examples/simple/pages/_document.js
@@ -1,94 +1,11 @@
 import { compose } from 'compose-middleware';
 import { nextI18NextMiddleware } from 'next-i18next-serverless/dist/commonjs/middlewares/next-i18next-middleware';
-import getConfig from 'next/config';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 import getNextI18NextInstance from '../i18n';
 
-// const NextI18Next = require('next-i18next-serverless').default;
-
-// const nextI18NextMiddleware = require('next-i18next-serverless/middleware');
-// const wrapSession = handler => withSession(handler, {});
-// const wrapDocument = handler => wrapSession(handler);
-
-// export const middleware = async ({ req, res }) => {
-//   // We need to remap the calling convention since nextjs's documentMiddleware takes ({req,res})
-//   // while normal api routes expect (req, res).
-//   await wrapDocument(() => {})(req, res);
-// };
-
-// const wrapI18n = async ({ req, res }) => {
-//   const { publicRuntimeConfig } = getConfig();
-
-//   const { PROJECT_ROOT } = publicRuntimeConfig;
-
-//   console.log('PROJECT_ROOT from i18n-next: ', PROJECT_ROOT);
-
-//   console.log('nextI18NextMiddleware: ', nextI18NextMiddleware);
-//   console.log('instance: ', instance);
-//   const middleware = compose(nextI18NextMiddleware(instance));
-
-//   const done = () => {
-//     console.log('done');
-//   };
-
-//   middleware(req, res, next => {
-//     return done();
-//   });
-// };
-
-/*
-interface OProps {
-  req: http.IncomingMessage;
-  res: http.ServerResponse;
-}
-*/
-
-const wrapI18n = (req, res) => {
-  const { publicRuntimeConfig } = getConfig();
-  console.log(
-    'instantiateTestMiddleware in publicRuntimeConfig: ',
-    publicRuntimeConfig
-  );
-
-  const instance = getNextI18NextInstance(getConfig());
-  console.log('instance in publicRuntimeConfig: ', instance);
-
-  const middleware = compose(nextI18NextMiddleware(instance));
-
-  const done = () => {
-    console.log('done');
-  };
-
-  middleware(req, res, next => {
-    return done();
-  });
-};
-
-export const middleware = async ({ req, res }) => {
-  const { publicRuntimeConfig } = getConfig();
-
-  const { PROJECT_ROOT } = publicRuntimeConfig;
-
-  console.log('PROJECT_ROOT from middleware: ', PROJECT_ROOT);
-
-  // We need to remap the calling convention since nextjs's documentMiddleware takes ({req,res})
-  // while normal api routes expect (req, res).
-  // await wrapI18n(req, res);
-};
-
-export const testMiddleware = () => {
-  // const middleware: Array<Handler<IncomingMessage, ServerResponse>> = [];
-
-  const fake = (_req, _res, next) => {
-    console.log('Fake middleware step');
-    next();
-  };
-  return [fake];
-};
-
-export const composeTestMiddleware = ({ req, res }) => middlewares => {
+export const composeTestMiddleware = (req, res) => middlewares => {
   const handler = compose(middlewares);
 
   const done = () => {
@@ -102,42 +19,14 @@ export const composeTestMiddleware = ({ req, res }) => middlewares => {
   return handler;
 };
 
-export const instantiateTestMiddleware = ({ req, res }) => {
-  const { publicRuntimeConfig } = getConfig();
-  console.log(
-    'instantiateTestMiddleware in publicRuntimeConfig: ',
-    publicRuntimeConfig
-  );
-
-  const instance = getNextI18NextInstance(getConfig());
-  console.log('instance in publicRuntimeConfig: ', instance);
-  composeTestMiddleware(req, res)(testMiddleware());
+export const instantiateTestMiddleware = (req, res) => {
+  const instance = getNextI18NextInstance;
+  composeTestMiddleware(req, res)(nextI18NextMiddleware(instance));
 };
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    // await wrapI18n(ctx);
-
-    // const middleware = compose(nextI18NextMiddleware(null));
-
-    // const done = () => {
-    //   console.log('done');
-    // };
-
-    // await middleware(req, res, next => {
-    //   return done();
-    // });
-
-    /*
-    const instance = getNextI18NextInstance(getConfig());
-
-    const a = nextI18NextMiddleware(instance);
-
-    composeTestMiddleware(ctx)();
-    */
-
-    // instantiateTestMiddleware(ctx);
-    wrapI18n(ctx.req, ctx.res);
+    instantiateTestMiddleware(ctx.req, ctx.res);
 
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
